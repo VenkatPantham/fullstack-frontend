313413fb2122a50e8ae02db257eb3717d1f194c8
VenkatPantham <venkatpantham5568@gmail.com>
2019-07-11 18:25:05 +0530

    comment rerendering issue solved

---
+++ src/components/AddReview.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import StarRatingComponent from "react-star-rating-component";
+import axios from "axios";
+import "./Review.css";
+let baseUrl = "https://aws.tomato.tk";
+
+class AddReview extends Component {
+  constructor(props) {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onStarClick = this.onStarClick.bind(this);
+    this.state = {
+      rating: 0
+    };
+  }
+
+  onStarClick(nextValue) {
+    this.setState({ rating: nextValue });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let form = event.target;
+    console.log(form);
+    axios
+      .post(`${baseUrl}/restaurants/${this.props.data}/review`, {
+        review: event.target.review.value,
+        rating: event.target.ratings.value
+      })
+      .then(rev => {
+        this.props.updated(rev);
+      });
+    form.reset();
+    this.setState({ rating: 0 });
+  }
+
+  render() {
+    return (
+      <div className="review_box">
+        <form onSubmit={this.handleSubmit} className="form-group review_add">
+          <h4 align="left">Write a Review</h4>
+          <input
+            className="form-control"
+            name="review"
+            type="text"
+            placeholder="Help other foodies by sharing your review ..."
+            required
+          />
+          <div className="rating">
+            <StarRatingComponent
+              name="ratings"
+              starCount={5}
+              value={this.state.rating}
+              onStarClick={this.onStarClick}
+            />
+          </div>
+          <button className="btn btn-success review_button">
+            Add Your Review
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddReview;
--- src/components/Review.js
+++ src/components/Review.js
@@ -2,42 +2,27 @@ import React, { Component } from "react";
 import axios from "axios";
 import NavigationBar from "./NavigationBar";
 import RestaurantDetails from "./RestaurantDetails";
-import StarRatingComponent from "react-star-rating-component";
+import AddReview from "./AddReview";
 import "./Review.css";
 let baseUrl = "https://aws.tomato.tk";
 
 class Review extends Component {
   constructor(props) {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = { restaurant: [], review: [] };
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-    let form = event.target;
-    await axios
-      .post(
-        `${baseUrl}/restaurants/${this.props.match.params.restaurantId}/review`,
-        {
-          review: event.target.review.value,
-          rating: event.target.rating.value
-        }
-      )
-      .then(rev => {
-        alert("Comment added successfully!");
-        axios.get(`${baseUrl}/users/${rev.data.userId}`).then(user => {
-          rev.data.user = user.data;
-          this.forceUpdate();
-        });
-        this.state.review.push(rev.data);
-        this.setState((this.state.review = this.state.review));
-      });
-    form.reset();
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   componentDidMount() {
     this.fetch();
+  }
+
+  handleUpdate(review) {
+    axios.get(`${baseUrl}/users/${review.data.userId}`).then(user => {
+      review.data.user = user.data;
+      this.forceUpdate();
+    });
+    this.setState(this.state.review.push(review.data));
   }
 
   async fetch() {
@@ -71,37 +56,12 @@ class Review extends Component {
         <NavigationBar />
         <div className="container review">
           {this.state.restaurant.map(item => (
-            <RestaurantDetails
-              data={item}
-              history={this.props.history}
-              reviews={this.state.review}
-            />
+            <RestaurantDetails data={item} reviews={this.state.review} />
           ))}
-          <div className="review_box">
-            <form
-              onSubmit={this.handleSubmit}
-              className="form-group review_add"
-            >
-              <h4 align="left">Write a Review</h4>
-              <input
-                className="form-control"
-                name="review"
-                type="text"
-                placeholder="Help other foodies by sharing your review ..."
-                required
-              />
-              <div className="rating">
-                <StarRatingComponent
-                  name="rating"
-                  starCount={5}
-                  required="required"
-                />
-              </div>
-              <button className="btn btn-success review_button">
-                Add Your Review
-              </button>
-            </form>
-          </div>
+          <AddReview
+            data={this.props.match.params.restaurantId}
+            updated={this.handleUpdate}
+          />
         </div>
       </div>
     );
